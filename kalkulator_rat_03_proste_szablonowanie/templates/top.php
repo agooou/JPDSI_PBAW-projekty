c59846654a67bbf17b9511aece94b14dddff3566
agooou <80220783+agooou@users.noreply.github.com>
2021-03-23 00:47:45 +0100

    Add files via upload

---
+++ kalkulator_rat_03_proste_szablonowanie/templates/top.php
@@ -0,0 +1,16 @@
+<header id="header" style="text-align: center;">
+    
+    
+                            <h1><strong>Kalkulator rat</strong></h1>
+                            
+                            
+                     <!--   <span class="avatar"><img id = "logo" src="<?php print(_APP_ROOT); ?>/images/logo.jpg" alt="Logo" /></span>
+
+                        <ul class="icons">
+                            <li><a href="#" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
+                            <li><a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
+                            <li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
+
+                            <li><a href="#" class="icon style2 fa-envelope-o"><span class="label">Email</span></a></li> -->
+                        </ul>
+                    </header>
