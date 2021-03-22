5857b557d8f270ed9a0d3039e1120f27e464fb66
agooou <80220783+agooou@users.noreply.github.com>
2021-03-23 00:49:38 +0100

    Delete kalkulator_rat_03_proste_szablonowanie directory

--- kalkulator_rat_03_proste_szablonowanie/templates/bottom.php
+++
@@ -1,6 +0,0 @@
-<footer id="footer">
-                        <div style="text-align: center;">
-                        <p><br/>Kalkulator rat na bazie gotowego szablonu 
-                        <p>&copy; Untitled. All rights reserved. Design: <a href="https://html5up.net/uploads/demos/story/https://html5up.net/uploads/demos/story/"</a>:)</p>
-                        </div>
-            </footer>
